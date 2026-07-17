3fa4c6b4b87a83de6b83a28709d2ce99e9dc9414
Shiva Hari Timilsena <sumantimilsena@gmail.com>
2026-07-17 11:39:06 +0900

    Add initail file for level C

---
+++ MelononShushi.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+//C062
+public class MelononShushi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int dishes = sc.nextInt();
+        
+        int melonCount = 0;      
+        int eatingCooldown = 0; 
+        
+        for (int i = 0; i < dishes; i++) {
+            String item = sc.next();
+            
+            if (eatingCooldown > 0) {
+                eatingCooldown--;
+            } else if (item.equals("melon")) {
+                melonCount++;
+                eatingCooldown = 9;
+            }
+        }
+        
+        System.out.println(melonCount);
+        
+        sc.close();
+    }
+}
