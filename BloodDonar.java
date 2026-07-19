fa7469cd818accb3009a19d0c426c7fd5c68e93c
Shiva Hari Timilsena <sumantimilsena@gmail.com>
2026-07-19 18:34:47 +0900

    Added level assignment for basic level and level 2

---
+++ BloodDonar.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class BloodDonar {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input age: ");
+        int age = sc.nextInt();
+        if(age>=18){
+            System.out.print("Input Weight: ");
+            int weight = sc.nextInt();
+            if(weight>50){
+                System.out.println("You can Donate");
+            }else{
+                System.out.println("You are not eligible");
+            }
+        }else{
+            System.out.println("Your age must be greater than 18");
+        }
+    }
+}
---
+++ PrizeGiveAwayCampaign.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class PrizeGiveAwayCampaign {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of participants:");
+        int participants = sc.nextInt();
+        System.out.println("Enter the value of X:");
+        int x = sc.nextInt();
+        System.out.println("Enter the value of Y:");
+        int y = sc.nextInt();
+
+        for(int i = 1; i <= participants; i++){
+            // Logic for determining prize winner would go here
+            if(i % x == 0 && i % y == 0){
+                System.out.println("AB");
+            } else if(i % x == 0){
+                System.out.println("A");
+            } else if(i % y == 0){
+                System.out.println("B");
+            } else {
+                System.out.println("N");
+            }
+        }
+    }
+}
